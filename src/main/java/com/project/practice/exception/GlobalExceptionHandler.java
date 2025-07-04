9a030c395ea28d1476bf1e3b893d67fa31f40133
Ishan R Tripathi <ishanrtripathi786@gmail.com>
2025-07-05 02:13:12 +0530

    Adding Docker file to run project on docker machine

---
+++ src/main/java/com/project/practice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.project.practice.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public void globalExceptionHandler() {
+        log.error("Exception occured");
+    }
+
+}
---
+++ src/main/java/com/project/practice/service/PracticeService.java
@@ -0,0 +1,9 @@
+package com.project.practice.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PracticeService {
+}
