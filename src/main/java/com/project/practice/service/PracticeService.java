c1f1976dcafc5a03b6a1053b86f47b054755551a
RiteshMishra <1705437@kiit.ac.in>
2025-07-05 02:22:36 +0530

    Added a greet endpoint in PracticeService

--- src/main/java/com/project/practice/service/PracticeService.java
+++ src/main/java/com/project/practice/service/PracticeService.java
@@ -2,8 +2,16 @@ package com.project.practice.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @Service
+@RestController
 public class PracticeService {
+
+    @GetMapping("/greet")
+    public void greet(){
+        System.out.println("Hello There!!");
+    }
 }
