1ad9105399352067b9bc84ef29027b008fa7ba0b
RiteshMishra <1705437@kiit.ac.in>
2025-07-08 01:03:25 +0530

    Singleton Pattern

---
+++ src/main/java/com/project/practice/Patterns/Logger.java
@@ -0,0 +1,25 @@
+package com.project.practice.Patterns;
+
+//Singleton logger
+public class Logger {
+    // volatile keyword makes the instance available to all threads once it's assigned.
+    private static volatile Logger instance;
+
+    //Private constructor so that no one else can create the Object of logger class
+    private Logger() {}
+
+    public static Logger getInstance() {
+        if(instance == null){
+            synchronized (Logger.class){
+                if(instance == null){
+                    instance = new Logger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void log(String msg){
+        System.out.println(msg);
+    }
+}
